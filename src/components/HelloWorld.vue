026807b03a8fe2ddf8cb402cda690043ee43845d
lilili001 <2861166132@qq.com>
2018-11-08 18:57:46 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+     this is helloworld component
+       <ul>
+          <li><router-link :to="{ name: 'counter'}">counter</router-link></li>
+          <li><router-link :to="{ name: 'todolist'}">todolist</router-link></li>
+          <li><router-link :to="{ name: 'portal'}">portal</router-link></li>
+          <li><router-link :to="{ name: 'fangkuan'}">放款</router-link></li>
+          <li><router-link :to="{ name: 'datatranser'}">dataTransfer</router-link></li>
+      </ul>
+
+      <mt-tabbar v-model="selected">
+          <mt-tab-item id="tab1">
+              tab1
+          </mt-tab-item>
+          <mt-tab-item id="tab2">
+              tab2
+          </mt-tab-item>
+          <mt-tab-item id="tab3">
+              tab3
+          </mt-tab-item>
+          <mt-tab-item id="tab4">
+              tab4
+          </mt-tab-item>
+      </mt-tabbar>
+
+      <mt-button type="default">default</mt-button>
+      <mt-button type="primary">primary</mt-button>
+      <mt-button type="danger">danger</mt-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+        selected:true
+    }
+  },
+  mounted(){
+
+  },
+    methods:{
+      fn1(){
+          alert()
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{text-align:center}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,77 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//import Mint from "miya-mint";
+
+import { Tabbar, TabItem } from 'miya-mint';
+
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+
+import { Button } from 'miya-mint';
+
+Vue.component(Button.name, Button);
+
+//Vue.use(Mint);
+import 'miya-mint/lib/style.css'
+import '../../iview/scss/_common.scss';
+
+import store from './store';
+import qs from 'qs';
+import axios from 'axios';
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false;
+
+var isFormData = function (obj) {
+    return  obj instanceof FormData ;
+};
+
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+
+    if (!isFormData(config.data) && config.method == 'post' ) {
+        config.data = qs.stringify(config.data)
+        config.headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
+    } 
+
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+Vue.prototype.$http.interceptors.response.use(
+
+    (response)=> {
+        var suffix = ' 请求状态:' + response.status+':'+response.statusText;
+        if (!(response.data.success) && response.data.message) {
+            info(response.data.message );
+        }
+        return response
+    },
+
+    (error) => {
+        var info = (text) => {
+            if (zero.$Message) zero.$Message.destroy();
+            zero.$Modal.error({
+                content: text
+            });
+        };
+        var err = error.response;
+        
+        //Do something with response error
+        return Promise.reject(error)
+});
+
+window._ = require('underscore');
+
+/* eslint-disable no-new */
+const zero = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
+});
