fb26041b624a9f553733b8ca0d23b2fd2d750219
lilili001 <2861166132@qq.com>
2018-11-21 20:16:00 +0800

    edit sts-auth.php

--- src/views/tibao/pages/upload.vue
+++ src/views/tibao/pages/upload.vue
@@ -4,7 +4,8 @@
                 :left-options="{backText: ''}"
                 @on-click-more="showMenus = true"
                 :right-options="{showMore: true}"
-        >附件信息</x-header>
+        >附件信息
+        </x-header>
         <div v-transfer-dom>
             <actionsheet :menus="menus" v-model="showMenus" show-cancel></actionsheet>
         </div>
@@ -15,8 +16,8 @@
                 <tab-item @on-item-click="onItemClick">车辆证明类</tab-item>
             </tab>
             <div class="tab-content">
-                <a  href="javascript:;" class="a-upload">
-                    <input type="file" id="file-selector" @change="fnChange(1)"  name="" class="file-item" />+
+                <a href="javascript:;" class="a-upload">
+                    <input type="file" id="file-selector" @change="fnChange(1)" name="" class="file-item"/>+
                 </a>
 
             </div>
@@ -35,10 +36,11 @@
     import Cell from '@mpack/components/cell/index.vue'
     import CellFormPreview from '@mpack/components/cell-form-preview/index.vue'
     import Group from '@mpack/components/group/index.vue'
-    import {Step,StepItem} from '@mpack/components/step'
-    import {Tabbar,TabbarItem} from '@mpack/components/tabbar'
+    import {Step, StepItem} from '@mpack/components/step'
+    import {Tabbar, TabbarItem} from '@mpack/components/tabbar'
     import resultCheck from '@/views/tibao/partials/result-check.vue'
-    import { Tab, TabItem } from '@mpack/components/tab'
+    import {Tab, TabItem} from '@mpack/components/tab'
+
     var sh = {
         Bucket: 'mt-1257971636',
         Region: 'ap-shanghai'
@@ -48,7 +50,7 @@
         getAuthorization: function (options, callback) {
             // 方法一、后端通过获取临时密钥给到前端，前端计算签名
             // var url = 'http://127.0.0.1:3000/sts';
-            var url = 'http://localhost/frontend_zujian/cos-js-sdk-v5/server/sts.php';
+            /*var url = 'http://localhost/frontend_zujian/cos-js-sdk-v5/server/sts.php';
             var xhr = new XMLHttpRequest();
             xhr.open('GET', url, true);
             xhr.onload = function (e) {
@@ -63,27 +65,60 @@
                     ExpiredTime: data.expiredTime,
                 });
             };
-            xhr.send();
+            xhr.send();*/
+
+
+            var method = (options.Method || 'get').toLowerCase();
+            var key = options.Key || '';
+            var query = options.Query || {};
+            var headers = options.Headers || {};
+            var pathname = key.indexOf('/') === 0 ? key : '/' + key;
+            // var url = 'http://127.0.0.1:3000/sts-auth';
+            //var url = '../server/yx-sts-auth.php';
+            var url = 'http://localhost/frontend_zujian/cos-js-sdk-v5/server/sts-auth.php';
+            var xhr = new XMLHttpRequest();
+            var data = {
+                method: method,
+                pathname: pathname,
+                query: query,
+                headers: headers,
+            };
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('content-type', 'application/json');
+            xhr.onload = function (e) {
+                try {
+                    var AuthData = JSON.parse(e.target.responseText);
+                } catch (e) {
+                }
+                callback({
+                    Authorization: AuthData.Authorization,
+                    XCosSecurityToken: AuthData.XCosSecurityToken,
+                });
+            };
+            xhr.send(JSON.stringify(data));
+
         }
+
+
     });
-    export default{
-        name:"vux-page",
-        data:function () {
+    export default {
+        name: "vux-page",
+        data: function () {
             return {
                 showMenus: false,
                 menus: {
                     menu1: 'Take Photo',
                     menu2: 'Choose from photos'
                 },
-                step1:1,
-                num : 2,
-                cos:null
+                step1: 1,
+                num: 2,
+                cos: null
             }
         },
         directives: {
             TransferDom
         },
-        components:{
+        components: {
             XHeader,
             Actionsheet,
             Card,
@@ -92,28 +127,28 @@
             Cell,
             CellFormPreview,
             Group,
-            Step,StepItem,
-            Tabbar,TabbarItem,
+            Step, StepItem,
+            Tabbar, TabbarItem,
             resultCheck,
             Tab, TabItem
         },
-        methods:{
-            onClick(){
+        methods: {
+            onClick() {
                 console.log(this)
             },
-            submitOrder(){
+            submitOrder() {
                 console.log('submit order')
             },
-            onItemClick (index) {
+            onItemClick(index) {
                 console.log('on item click:', index)
             },
-            fnChange(v){
+            fnChange(v) {
 
                 var file = event.target.files[0];
                 cos.putObject({
                     Bucket: sh.Bucket, // Bucket 格式：test-1250000000
                     Region: sh.Region,
-                    Key: '/dir/'+file.name, /* 必须 */
+                    Key: '/dir/' + file.name, /* 必须 */
                     Body: file,
                     TaskReady: function (tid) {
                         //TaskId = tid;
@@ -157,7 +192,7 @@
         *zoom: 1
     }
 
-    .a-upload  input {
+    .a-upload input {
         position: absolute;
         font-size: 100px;
         right: 0;
